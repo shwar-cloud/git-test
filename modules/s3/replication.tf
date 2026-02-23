c28eb4e7f26fb2bbc90b295af566e40b12d6c498
shwar-cloud <vootlogin1@gmail.com>
2026-02-23 11:14:52 +0530

    Update replication.tf

--- modules/s3/replication.tf
+++ modules/s3/replication.tf
@@ -1,31 +1,34 @@
 resource "aws_s3_bucket_replication_configuration" "sclr_replication" {
   bucket = aws_s3_bucket.sclr_source.id
   role   = aws_iam_role.sclr_replication_role.arn
+  depends_on = [
+    aws_s3_bucket_versioning.source,
+    aws_s3_bucket_versioning.destination,
+    aws_s3_bucket_server_side_encryption_configuration.source_encryption,
+    aws_s3_bucket_server_side_encryption_configuration.destination_encryption
+  ]
 
-  rule {
-    id     = "sclr-replication-rule"
+rule {
+    #id     = "ReplicateKMSOnly"
     status = "Enabled"
+    filter {}
 
+    # Only replicate KMS-encrypted objects
     source_selection_criteria {
-      sse_kms_encrypted_objects { 
+      sse_kms_encrypted_objects {
         status = "Enabled"
-         }
+      }
     }
 
-    destination {
+  destination {
       bucket        = aws_s3_bucket.sclr_destination.arn
       storage_class = "STANDARD"
 
       encryption_configuration {
         replica_kms_key_id = aws_kms_key.sclr_destination_kms.arn
       }
-    }
+    } 
   }
+} 
 
-  depends_on = [
-    aws_s3_bucket.sclr_destination,
-    aws_s3_bucket_versioning.sclr_source_versioning,
-    aws_s3_bucket_versioning.sclr_destination_versioning,
-    aws_iam_role_policy.sclr_replication_policy
-  ]
-}
+ 
