62f45b54eff08108a8c78debfd858a8543f63277
shwar-cloud <vootlogin1@gmail.com>
2026-02-23 15:40:16 +0530

    Update iam.tf

--- modules/s3/iam.tf
+++ modules/s3/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "sclr_replication_role" {
-  name = "sclr-replication-role"
+  name = "sclr_replication_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
