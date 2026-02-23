34886aa15f23ee4d410374a2339bb46a53153ca9
shwar-cloud <vootlogin1@gmail.com>
2026-02-23 11:22:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,16 @@
-# PROVIDERS
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "dr"
-  region = "us-west-2"
-}
-
 #  S3 caller module
 
 module "s3" {
   source = "./modules/s3"
 
-  source_bucket_name       = "sclrsourcebucket"
-  destination_bucket_name  = "sclrdestinationbucket"
-  
-  providers = {
+ providers = {
     aws    = aws       # Default provider 
     aws.dr = aws.dr    # Alias provider 
+  
   }
+
+  source_bucket_name       = "sclrsourcebucket"
+  destination_bucket_name  = "sclrdestinationbucket"
+  source_region           = var.source_region
+  destination_region      = var.destination_region
 }
