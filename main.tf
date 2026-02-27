97d9ec1cd83eeb645e79c0b726d9b37e5a629a50
Aarthi <vootlogin1@gmail.com>
2026-02-27 10:00:32 +0530

    Resolved merge conflicts and cleaned Terraform files

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module "s3" {
   source = "./modules/s3"
 
@@ -12,21 +11,3 @@ module "s3" {
   source_bucket_name      = var.source_bucket_name
   destination_bucket_name = var.destination_bucket_name
 }
-=======
-#  S3 caller module
-
-module "s3" {
-  source = "./modules/s3"
-
- providers = {
-    aws    = aws       # Default provider 
-    aws.dr = aws.dr    # Alias provider 
-  
-  }
-
-  source_bucket_name       = "sclrsourcebucket"
-  destination_bucket_name  = "sclrdestinationbucket"
-  source_region           = var.source_region
-  destination_region      = var.destination_region
-}
->>>>>>> 7a2216ea509432eef42f2c9204d95f6393d90765
--- provider.tf
+++ provider.tf
@@ -5,8 +5,4 @@ provider "aws" {
 provider "aws" {
   alias  = "dr"
   region = var.destination_region
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 7a2216ea509432eef42f2c9204d95f6393d90765
